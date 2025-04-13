42e9087fc27aa55278a92f9b35e9fa5c19365ca3
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-04-13 17:46:06 +0330

    kntr tracking code

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import './assets/fonts/Samim.css'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
+import TrackingPlugin from './plugins/tracker.js';
 import './assets/css/main.css'
+
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
@@ -10,6 +12,13 @@ Vue.use(VueRouter)
 var router = new VueRouter({
   mode: 'history',
   routes: []
+});
+
+Vue.use(TrackingPlugin, {
+  id: "1",
+  status: "",
+  debug: true, // optional
+  router // optional
 });
 
 new Vue({
